a945db263ab0c89604a928be67b48f52ca6089b9
chep <vcheplev42@gmail.com>
2023-03-23 18:41:47 +0400

    mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config({ path: ".env" });
+require("dotenv").config();
 
-const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const { API_URL, PRIVATE_KEY, BSCSCAN_API_KEY } = process.env;
+//* Default Template for Reference
 
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "polygon_mumbai",
   networks: {
-    goerli: {
-      url: QUICKNODE_HTTP_URL,
-      accounts: [PRIVATE_KEY],
+    hardhat: {
+      chainId: 31337
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
     },
   },
 };
+
+
+// Configuration
+/*
+  solidity - The version of solidity compiler
+  defaultNetwork - The Default network to run (Without running --network-name)
+  networks - Object which contains the network information
+  etherscan - Object to fill in EtherScan Information for contract verification
+*/
+// module.exports = {
+//   solidity: "0.8.9",
+// };
